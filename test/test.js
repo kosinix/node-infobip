2575c6c3fc035c0802d2fde40f4043f83d2a270b
kosinix <kosinix@users.noreply.github.com>
2019-06-11 14:28:44 +0800

    Add test

--- test/test.js
+++ test/test.js
@@ -2,10 +2,39 @@ let infobip = require('../index');
 
 let main = async () => {
     console.log(await infobip.status())
+
+    let settings = new infobip.Settings()
+    settings.authorize('Basic', 'username', 'pass')
+    // console.log(await settings.getApiKeys())
+    // console.log(await settings.getApiKey('key'))
+    // console.log(await settings.getApiKeyByPublicKey('public-api-key'))
+    // console.log(await settings.newApiKey({
+    //     "name": "Api key 1",
+    //     "allowedIPs": [],
+    //     "permissions": [
+    //         "ALL"
+    //     ]
+    // }))
+    // console.log(await settings.updateApiKey('abc', {
+    //     "name": "Api key 2",
+    //     "allowedIPs": ['127.0.0.1']
+    // }))
+    // console.log(await settings.getApiKeyByName('live-server'))
+
 }
 
-main().then((result)=>{
+main().then((result) => {
     console.log('Test done.')
-}).catch((err)=>{
-    console.log(err)
+}).catch((err) => {
+    if (err.response) {
+        if (err.response.data) {
+            console.log(err.response.data)
+        } else {
+            console.log(err.response)
+        }
+    } else if (err.data) {
+        console.log(err.data)
+    } else {
+        console.log(err)
+    }
 })
