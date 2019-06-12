d5c77ef84e15dd6870c49ad0cfa39fa8e696ab94
kosinix <kosinix@users.noreply.github.com>
2019-06-12 21:02:13 +0800

    Break index.js into individual files

---
+++ service/SMS.js
@@ -0,0 +1,125 @@
+//// Core modules
+
+//// External modules
+
+//// Modules
+const authorize = require('./../helpers').authorize;
+const trimError = require('./../helpers').trimError;
+
+
+/**
+ * Class for SMS service
+ * 
+ * @example 
+ * // Include module
+ * let infobip = require('node-infobip');
+ * 
+ * // Instantiate SMS class. Specify Sender ID and Base URL
+ * let sms = new infobip.SMS('CompanyA', 'https://api.infobip.com');
+ * 
+ * // Basic authorization
+ * sms.authorize('Basic', 'username', 'password');
+ * 
+ * // API key authorization
+ * sms.authorize('App', 'public-api-key');
+ * 
+ * // Token authorization
+ * sms.authorize('IBSSO', 'token');
+ * 
+ * // Send single text
+ * await sms.single('631234567890', 'Hello there!');
+ * 
+ * // Send single text to multiple recipients
+ * await sms.single(['631234567890', '631234567891'], 'Hello there!');
+ * 
+ */
+class SMS {
+    /**
+     * Instantiate SMS class
+     * 
+     * @param {string} defaultFrom Represents a sender ID which can be alphanumeric or numeric
+     * @param {string} baseUrl Infobip personal base URL
+     * @param {number} version API version 1 or 2
+     * @param {string} contentType The type of data the API returns. Values: "json" or "xml"
+     */
+    constructor(defaultFrom = 'INFO', baseUrl = 'https://api.infobip.com', version = 1, contentType = 'json') {
+        if (version < 1 || version > 2) {
+            throw new Error('Invalid version number.')
+        }
+        if (!(contentType === 'json' || contentType === 'xml')) {
+            throw new Error('Invalid content type.')
+        }
+        this.baseUrl = baseUrl;
+        this.version = version;
+        this.defaultFrom = defaultFrom;
+        this.contentType = contentType;
+        this.axios = null;
+    }
+
+    /**
+     * Authorize API calls
+     * 
+     * @param {string} authType 
+     * @param {string} tokenKeyOrUsername 
+     * @param {string} password 
+     */
+    authorize(authType, tokenKeyOrUsername, password = '') {
+        this.axios = authorize(authType, tokenKeyOrUsername, password, this.contentType)
+    }
+
+    /**
+     * Send single SMS. 
+     * 
+     * The maximum length of one message is 160 characters for the GSM7 standard or 70 characters for Unicode encoded messages.
+     * 
+     * @param {string|Array} to Destination addresses must be in international format (example: 41793026727)
+     * @param {string} text Text of the message that will be sent.
+     * @param {string} from Represents sender ID and it can be alphanumeric or numeric. Alphanumeric sender ID length should be between 3 and 11 characters (example: CompanyName). Numeric sender ID length should be between 3 and 14 characters.
+     * 
+     * @returns {Object} Axios response.data
+     */
+    async single(to, text, from = '') {
+        try {
+            if (!this.axios) {
+                throw new Error('Unauthorized API call.')
+            }
+            if (!from) {
+                from = this.defaultFrom;
+            }
+            let response = await this.axios.post(
+                `${this.baseUrl}/sms/${this.version}/text/single`,
+                {
+                    from: from,
+                    to: to,
+                    text: text
+                }
+            );
+            return response.data;
+        } catch (err) {
+            throw trimError(err)
+        }
+    }
+
+
+    /**
+     * Getting a report via message ID
+     * 
+     * @param {string} messageId The ID that uniquely identifies the message sent.
+     * 
+     * @returns {Object} Axios response.data
+     */
+    async getReportByMessageId(messageId) {
+        try {
+            if (!this.axios) {
+                throw new Error('Unauthorized API call.')
+            }
+            let response = await this.axios.get(`${this.baseUrl}/sms/${this.version}/reports?messageId=${messageId}`);
+            return response.data;
+        } catch (err) {
+            throw trimError(err)
+        }
+    }
+
+}
+
+module.exports = SMS
---
+++ service/Settings.js
@@ -0,0 +1,234 @@
+//// Core modules
+
+//// External modules
+
+//// Modules
+const authorize = require('./../helpers').authorize;
+const trimError = require('./../helpers').trimError;
+
+/**
+ * Class for Account service use in managing API keys 
+ * 
+ * @example 
+ * // Include module
+ * let infobip = require('node-infobip');
+ * 
+ * // Instantiate Settings class
+ * let settings = new infobip.Settings()
+ * 
+ * // Authorize using Basic. We can swap this later with App authorization
+ * settings.authorize('Basic', 'username', 'password')
+ * 
+ * // List all API keys
+ * console.log(await settings.getApiKeys())
+ * 
+ * // Get API key with property key "abc"
+ * console.log(await settings.getApiKey('abc'))
+ * 
+ * // Get API key with property publicApiKey "abc"
+ * console.log(await settings.getApiKeyByPublicKey('abc'))
+ * 
+ * // Get API key with property name "live-server"
+ * console.log(await settings.getApiKeyByName('live-server'))
+ * 
+ */
+class Settings {
+
+    /**
+     * Instantiate Settings class
+     * 
+     * @param {string} accountKey Key used to uniquely identify the account. Use _ as parameter value for your current account or account key for sub accounts.
+     * @param {string} baseUrl Infobip personal base URL
+     * @param {number} version API version 1 or 2
+     * @param {string} contentType The type of data the API returns. Values: "json" or "xml"
+     */
+    constructor(accountKey = '_', baseUrl = 'https://api.infobip.com', version = 1, contentType = 'json') {
+        if (version < 1 || version > 2) {
+            throw new Error('Invalid version number.')
+        }
+        if (!(contentType === 'json' || contentType === 'xml')) {
+            throw new Error('Invalid content type.')
+        }
+        this.baseUrl = baseUrl;
+        this.version = version;
+        this.accountKey = accountKey;
+        this.contentType = contentType;
+        this.axios = null;
+    }
+
+    /**
+     * Authorize API calls
+     * 
+     * @param {string} authType 
+     * @param {string} tokenKeyOrUsername 
+     * @param {string} password 
+     */
+    authorize(authType, tokenKeyOrUsername, password = '') {
+        this.axios = authorize(authType, tokenKeyOrUsername, password, this.contentType)
+    }
+
+    /**
+     * List all API keys
+     * 
+     * @param {string} enabled Filter enabled keys. Values: "true" or "false".
+     * 
+     * @returns {Object}
+     * @throws {Error}
+     */
+    async getApiKeys(enabled = '') {
+        try {
+            if (!this.axios) {
+                throw new Error('Unauthorized API call.')
+            }
+
+            let endPoint = `${this.baseUrl}/settings/${this.version}/accounts/${this.accountKey}/api-keys`
+
+            if (enabled) {
+                endPoint += `?enabled=${enabled}`
+            }
+            let response = await this.axios.get(endPoint);
+            return response.data;
+        } catch (err) {
+            throw trimError(err)
+        }
+    }
+
+
+    /**
+     * Get an API key by its key
+     * 
+     * @param {string} key Key (unique ID) of the API key.
+     * 
+     * @returns {Object}
+     * @throws {Error}
+     */
+    async getApiKey(key) {
+        try {
+            if (!this.axios) {
+                throw new Error('Unauthorized API call.')
+            }
+            if (!key) {
+                throw new Error('Please provide a key.')
+            }
+
+            let endPoint = `${this.baseUrl}/settings/${this.version}/accounts/${this.accountKey}/api-keys/${key}`
+
+            let response = await this.axios.get(endPoint);
+            return response.data;
+        } catch (err) {
+            throw trimError(err)
+        }
+    }
+
+
+    /**
+     * Get an API key by its public API key
+     * 
+     * @param {string} key Public API key of the API key.
+     * 
+     * @returns {Object}
+     * @throws {Error}
+     */
+    async getApiKeyByPublicKey(key) {
+        if (!this.axios) {
+            throw new Error('Unauthorized API call.')
+        }
+        if (!key) {
+            throw new Error('Please provide a key.')
+        }
+
+        let endPoint = `${this.baseUrl}/settings/${this.version}/accounts/${this.accountKey}/api-keys?publicApiKey=${key}`
+
+        let response = await this.axios.get(endPoint);
+        return response.data;
+    }
+
+    /**
+     * Get an API key by its name
+     * 
+     * @param {string} name Name of the API key.
+     * 
+     * @returns {Object}
+     * @throws {Error}
+     */
+    async getApiKeyByName(name) {
+        if (!this.axios) {
+            throw new Error('Unauthorized API call.')
+        }
+        if (!name) {
+            throw new Error('Please provide a name.')
+        }
+
+        let endPoint = `${this.baseUrl}/settings/${this.version}/accounts/${this.accountKey}/api-keys?name=${name}`
+
+        let response = await this.axios.get(endPoint);
+        return response.data;
+    }
+
+    /**
+     * Create an API key
+     * 
+     * @param {Object} params The API key properties
+     * 
+     * @returns {Object}
+     * @throws {Error}
+     * 
+     * @example
+     * 
+     * console.log(await settings.newApiKey({
+     *  "name": "Api key 1",
+     *  "allowedIPs": [],
+     *  "permissions": [
+     *      "ALL"
+     *  ]
+     * }))
+     */
+    async newApiKey(params) {
+        if (!this.axios) {
+            throw new Error('Unauthorized API call.')
+        }
+        if (!params) {
+            throw new Error('Please provide params.')
+        }
+
+        let endPoint = `${this.baseUrl}/settings/${this.version}/accounts/${this.accountKey}/api-keys`
+
+        let response = await this.axios.post(endPoint, params);
+        return response.data;
+    }
+
+    /**
+     * Update an API key
+     * 
+     * @param {string} key Key (unique ID) of the API key.
+     * @param {Object} params The API key properties to update
+     * 
+     * @returns {Object}
+     * @throws {Error}
+     * 
+     * @example
+     * 
+     * console.log(await settings.updateApiKey('abc', {
+     *  "name": "Api key 2",
+     *  "allowedIPs": ['127.0.0.1']
+     * }))
+     */
+    async updateApiKey(key, params) {
+        if (!this.axios) {
+            throw new Error('Unauthorized API call.')
+        }
+        if (!key) {
+            throw new Error('Please provide a key.')
+        }
+        if (!params) {
+            throw new Error('Please provide params.')
+        }
+
+        let endPoint = `${this.baseUrl}/settings/${this.version}/accounts/${this.accountKey}/api-keys/${key}`
+
+        let response = await this.axios.put(endPoint, params);
+        return response.data;
+    }
+}
+
+module.exports = Settings
