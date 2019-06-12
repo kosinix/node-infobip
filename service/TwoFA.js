ae7f816f9331081c6a9af33b7e3e25ff1f2fb6e8
kosinix <kosinix@users.noreply.github.com>
2019-06-12 22:50:40 +0800

    Add 2FA message templates and pin code over sms

--- service/TwoFA.js
+++ service/TwoFA.js
@@ -27,9 +27,9 @@ class TwoFA {
     /**
      * Instantiate class
      * 
-     * @param {string} baseUrl Infobip personal base URL
-     * @param {number} version API version 1 or 2
-     * @param {string} contentType The type of data the API returns. Values: "json" or "xml"
+     * @param {string} baseUrl Infobip personal base URL.
+     * @param {number} version API version 1 or 2.
+     * @param {string} contentType The type of data the API returns. Values: "json" or "xml".
      */
     constructor(baseUrl = 'https://api.infobip.com', version = 2, contentType = 'json') {
         if (version < 1 || version > 2) {
@@ -83,7 +83,7 @@ class TwoFA {
     /**
      * Get a 2FA application by its applicationId
      * 
-     * @param {string} applicationId Uunique ID of the app.
+     * @param {string} applicationId Unique ID of the app.
      * 
      * @returns {Object}
      * @throws {Error}
@@ -113,7 +113,7 @@ class TwoFA {
     /**
      * Create an application
      * 
-     * @param {Object} params The API key properties
+     * @param {Object} params The app properties.
      * 
      * @returns {Object}
      * @throws {Error}
@@ -151,10 +151,10 @@ class TwoFA {
      * Update an app
      * 
      * @param {string} applicationId Unique ID of the app.
-     * @param {Object} params The API key properties to update
+     * @param {Object} params The app properties to update.
      * 
      * @returns {Object}
-     * @throws {Error} Unauthorized API call
+     * @throws {Error} Unauthorized API call.
      * 
      * @example
      * 
@@ -177,6 +177,229 @@ class TwoFA {
         let endPoint = `${this.baseUrl}/2fa/${this.version}/applications/${applicationId}`
 
         let response = await this.axios.put(endPoint, params);
+        return response.data;
+    }
+
+    /**
+     * List all templates
+     * 
+     * @param {string} applicationId Unique ID of the app.
+     * @param {Object} version The API version to use. If set to "", uses the instance version.
+     * 
+     * @returns {Object}
+     * @throws {Error}
+     * 
+     * @example
+     * console.log(await twoFA.getMessageTemplates())
+     */
+    async getMessageTemplates(applicationId, version = 1) {
+        try {
+            if (!this.axios) {
+                throw new Error('Unauthorized API call.')
+            }
+            if (!applicationId) {
+                throw new Error('Please provide an applicationId.')
+            }
+            if (!version) {
+                version = this.version
+            }
+            let endPoint = `${this.baseUrl}/2fa/${version}/applications/${applicationId}/messages`
+
+            let response = await this.axios.get(endPoint);
+            return response.data;
+        } catch (err) {
+            throw trimError(err)
+        }
+    }
+
+    /**
+     * Create a message template
+     * 
+     * @param {string} applicationId Unique ID of the app.
+     * @param {Object} params The template properties.
+     * @param {Object} version The API version to use. If set to "", uses the instance version.
+     * 
+     * @returns {Object}
+     * @throws {Error}
+     * 
+     * @example
+     * 
+     * console.log(await twoFA.newMessageTemplate('E39FA9F9983246FEEE938A70FE0C94BD', {
+     *     "pinType": "NUMERIC",
+     *     "pinPlaceholder": "<pin>",
+     *     "messageText": "<pin> is your Verification Code",
+     *     "pinLength": 6,
+     *     "senderId": "Infobip",
+     *     "language": "en",
+     *     "repeatDTMF": "1#",
+     *     "speechRate": 1
+     * }))
+     */
+    async newMessageTemplate(applicationId, params, version = 1) {
+        if (!this.axios) {
+            throw new Error('Unauthorized API call.')
+        }
+        if (!applicationId) {
+            throw new Error('Please provide an applicationId.')
+        }
+        if (!params) {
+            throw new Error('Please provide params.')
+        }
+        if (!version) {
+            version = this.version
+        }
+
+        let endPoint = `${this.baseUrl}/2fa/${version}/applications/${applicationId}/messages`
+
+        let response = await this.axios.post(endPoint, params);
+        return response.data;
+    }
+
+    /**
+     * Update a message template
+     * 
+     * @param {string} applicationId Unique ID of the app.
+     * @param {Object} params The template properties.
+     * @param {Object} version The API version to use. If set to "", uses the instance version.
+     * 
+     * 
+     * @returns {Object}
+     * @throws {Error} Unauthorized API call.
+     * 
+     * @example
+     * 
+     * console.log(await twoFA.updateMessageTemplate('E39FA9F9983246FEEE938A70FE0C94BD', 'B7F6CDDC480C7902D2F5DE4EB1C37E39', {
+     *     "pinType": "NUMERIC",
+     *     "pinPlaceholder": "<pin>",
+     *     "messageText": "<pin> is your Verification Code",
+     *     "pinLength": 6,
+     *     "senderId": "Infobip",
+     *     "language": "en",
+     *     "repeatDTMF": "1#",
+     *     "speechRate": 1
+     * }))
+     */
+    async updateMessageTemplate(applicationId, messageId, params, version = 1) {
+        if (!this.axios) {
+            throw new Error('Unauthorized API call.')
+        }
+        if (!applicationId) {
+            throw new Error('Please provide an applicationId.')
+        }
+        if (!messageId) {
+            throw new Error('Please provide a messageId.')
+        }
+        if (!params) {
+            throw new Error('Please provide params.')
+        }
+        if (!version) {
+            version = this.version
+        }
+
+        let endPoint = `${this.baseUrl}/2fa/${version}/applications/${applicationId}/messages/${messageId}`
+
+        let response = await this.axios.put(endPoint, params);
+        return response.data;
+    }
+
+    /**
+     * Send a PIN code over SMS
+     * 
+     * @param {Object} params The pin properties.
+     * @param {Object} version The API version to use. If set to "", uses the instance version.
+     * 
+     * @returns {Object}
+     * @throws {Error}
+     * 
+     * @example
+     * 
+     * console.log(await twoFA.sendPin({
+     *  "applicationId": "E39FA9F9983246FEEE938A70FE0C94BD",
+     *  "messageId": "B7F6CDDC480C7902D2F5DE4EB1C37E39",
+     *  "from": "InfoSMS",
+     *  "to": "41793026727"
+     * }))
+     */
+    async sendPin(params, version = 1) {
+        if (!this.axios) {
+            throw new Error('Unauthorized API call.')
+        }
+        if (!params) {
+            throw new Error('Please provide params.')
+        }
+        if (!version) {
+            version = this.version
+        }
+
+        let endPoint = `${this.baseUrl}/2fa/${version}/pin`
+
+        let response = await this.axios.post(endPoint, params);
+        return response.data;
+    }
+
+    /**
+     * Resend a PIN code over SMS
+     * 
+     * @param {string} pinId Unique ID of the pin.
+     * @param {Object} version The API version to use. If set to "", uses the instance version.
+     * 
+     * @returns {Object}
+     * @throws {Error}
+     * 
+     * @example
+     * 
+     * console.log(await twoFA.resendPin('C69BCA07517DFB8F850EC9751B36B54B'))
+     */
+    async resendPin(pinId, version = 1) {
+        if (!this.axios) {
+            throw new Error('Unauthorized API call.')
+        }
+        if (!pinId) {
+            throw new Error('Please provide a pinId.')
+        }
+        if (!version) {
+            version = this.version
+        }
+
+        let endPoint = `${this.baseUrl}/2fa/${version}/pin/${pinId}/resend`
+
+        let response = await this.axios.post(endPoint);
+        return response.data;
+    }
+
+    /**
+     * Verify a PIN code
+     * 
+     * @param {string} pinId Unique ID of the pin.
+     * @param {string} pin The pin code to verify.
+     * @param {Object} version The API version to use. If set to "", uses the instance version.
+     * 
+     * @returns {Object}
+     * @throws {Error}
+     * 
+     * @example
+     * 
+     * console.log(await twoFA.verifyPin('C69BCA07517DFB8F850EC9751B36B54B', '123456'))
+     */
+    async verifyPin(pinId, pin, version = 1) {
+        if (!this.axios) {
+            throw new Error('Unauthorized API call.')
+        }
+        if (!pinId) {
+            throw new Error('Please provide a pinId.')
+        }
+        if (!pin) {
+            throw new Error('Please provide a pin.')
+        }
+        if (!version) {
+            version = this.version
+        }
+
+        let endPoint = `${this.baseUrl}/2fa/${version}/pin/${pinId}/verify`
+
+        let response = await this.axios.post(endPoint, {
+            pin: pin
+        });
         return response.data;
     }
 }
